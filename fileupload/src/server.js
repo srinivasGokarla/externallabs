497807fe4fc4483c488aa7c6185bff07099ce42d
srinivasGokarla <srinivasgokarla@gmail.com>
2022-11-15 18:05:26 +0530

    complete

--- fileupload/src/server.js
+++ fileupload/src/server.js
@@ -2,8 +2,8 @@ const app = require("./index")
 
 const connect = require("./configs/db")
 
-app.listen(3232,  async function() {
+app.listen(3333,  async function() {
 
     await connect()
-    console.log("listening on port 3232")
+    console.log("listening on port 3333")
 })
